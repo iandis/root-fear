2f2338e0e66b2f63955330b3ed430c1b08a5e7ed
Iandi Santulus <iandisantulusn@gmail.com>
2022-06-10 17:29:24 +0700

    feat(android): implement `isRooted`

--- android/src/main/kotlin/app/iandis/root_fear/RootFearPlugin.kt
+++ android/src/main/kotlin/app/iandis/root_fear/RootFearPlugin.kt
@@ -1,35 +1,58 @@
 package app.iandis.root_fear
 
+import android.util.Log
 import androidx.annotation.NonNull
+import com.scottyab.rootbeer.RootBeer
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-/** RootFearPlugin */
-class RootFearPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class RootFearPlugin : FlutterPlugin, MethodCallHandler {
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "root_fear")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    companion object {
+        private const val _channelName: String = "root-fear-plugin"
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    private lateinit var channel: MethodChannel
+
+    private var _backgroundExecutor: ExecutorService? = null
+
+    private var _rootBeer: RootBeer? = null
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, _channelName)
+        channel.setMethodCallHandler(this)
+        _rootBeer = RootBeer(flutterPluginBinding.applicationContext)
+        _backgroundExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        try {
+            when (call.method) {
+                "isRooted" -> _checkIsRooted { result.success(it) }
+                else -> result.notImplemented()
+            }
+        } catch (e: Throwable) {
+            Log.e(_channelName, e.message ?: "Unknown error.", e.cause)
+            result.error("ROOT_FEAR_UNKNOWN_ERROR", e.message, e.cause)
+        }
+
+    }
+
+    private fun _checkIsRooted(onResult: (Boolean) -> Unit) = _backgroundExecutor?.execute {
+        val isRooted: Boolean = _rootBeer?.isRooted ?: false
+        onResult(isRooted)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        _backgroundExecutor?.shutdown()
+        _backgroundExecutor = null
+        _rootBeer = null
+    }
 }
