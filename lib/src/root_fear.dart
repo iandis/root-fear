9a03eaba68cb146d3bccbc964d4dc4b191fbc1be
Iandi Santulus <iandisantulusn@gmail.com>
2022-07-08 01:53:34 +0700

    docs: add comment on `RootFear` default constructor

--- lib/src/root_fear.dart
+++ lib/src/root_fear.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:root_fear/src/root_fear_android.dart';
 
 abstract class RootFear {
+  /// Returns a singleton instance of [RootFear]
   factory RootFear() {
     if (Platform.isAndroid) {
       return rootFearInstance;
