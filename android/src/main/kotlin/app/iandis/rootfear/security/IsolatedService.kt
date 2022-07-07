25bd70055ae9bc3ed7c005c21bba220063745778
Iandi Santulus <iandisantulusn@gmail.com>
2022-07-08 01:44:08 +0700

    fix: `isMagiskPresent` should be overriden and should be a method instead of a getter

--- android/src/main/kotlin/app/iandis/rootfear/security/IsolatedService.kt
+++ android/src/main/kotlin/app/iandis/rootfear/security/IsolatedService.kt
@@ -8,69 +8,66 @@ import io.flutter.Log
 import java.io.*
 
 internal class IsolatedService : Service() {
-    override fun onBind(intent: Intent): IBinder {
-        return mBinder
+    companion object {
+        private val blackListedMountPaths: Array<String> =
+            arrayOf("magisk", "core/mirror", "core/img")
+        private val TAG: String = IsolatedService::class.java.simpleName
     }
 
-    private val mBinder: IIsolatedService.Stub = object : IIsolatedService.Stub() {
+    override fun onBind(intent: Intent): IBinder = StubServiceBinder
+
+    private object StubServiceBinder : IIsolatedService.Stub() {
         /*Incase the java calls are hooked, there is 1 more level
                     of check in the native to detect if the same blacklisted paths are
                     found in the proc maps along with checks for su files when accessed
                     from native.Native functions can also be hooked.But requires some effort
                     if it is properly obfuscated and syscalls are used in place of libc calls
                      */
-        //Log.d(TAG, "MountPath:"+ str);
-        val isMagiskPresent: Boolean
-            get() {
-                Log.d(TAG, "Isolated UID:" + Os.getuid())
-                var isMagiskPresent = false
-                val file = File("/proc/self/mounts")
-                try {
-                    val fis = FileInputStream(file)
-                    val reader = BufferedReader(InputStreamReader(fis))
-                    var str: String
-                    var count = 0
-                    while (reader.readLine().also { str = it } != null && count == 0) {
-                        //Log.d(TAG, "MountPath:"+ str);
-                        for (path in blackListedMountPaths) {
-                            if (str.contains(path)) {
-                                Log.d(
-                                    TAG,
-                                    "Blacklisted Path found $path"
-                                )
-                                count++
-                                break
-                            }
+        override fun isMagiskPresent(): Boolean {
+            Log.d(TAG, "Isolated UID:" + Os.getuid())
+            var isMagiskPresent = false
+            val file = File("/proc/self/mounts")
+            try {
+                val fis = FileInputStream(file)
+                val reader = BufferedReader(InputStreamReader(fis))
+                var str: String
+                var count = 0
+                while (reader.readLine().also { str = it } != null && count == 0) {
+                    //Log.d(TAG, "MountPath:"+ str);
+                    for (path in blackListedMountPaths) {
+                        if (str.contains(path)) {
+                            Log.d(
+                                TAG,
+                                "Blacklisted Path found $path"
+                            )
+                            count++
+                            break
                         }
                     }
-                    reader.close()
-                    fis.close()
-                    Log.d(TAG, "Count of detected paths $count")
-                    if (count > 0) {
-                        Log.d(TAG, "Found magisk in atleast 1 mount path ")
-                        isMagiskPresent = true
-                    } else {
-                        /*Incase the java calls are hooked, there is 1 more level
+                }
+                reader.close()
+                fis.close()
+                Log.d(TAG, "Count of detected paths $count")
+                if (count > 0) {
+                    Log.d(TAG, "Found magisk in atleast 1 mount path ")
+                    isMagiskPresent = true
+                } else {
+                    /*Incase the java calls are hooked, there is 1 more level
                     of check in the native to detect if the same blacklisted paths are
                     found in the proc maps along with checks for su files when accessed
                     from native.Native functions can also be hooked.But requires some effort
                     if it is properly obfuscated and syscalls are used in place of libc calls
                      */
-                        isMagiskPresent = Native.isMagiskPresentNative
-                        Log.d(
-                            TAG,
-                            "Found Magisk in Native $isMagiskPresent"
-                        )
-                    }
-                } catch (e: IOException) {
-                    e.printStackTrace()
+                    isMagiskPresent = Native.isMagiskPresentNative
+                    Log.d(
+                        TAG,
+                        "Found Magisk in Native $isMagiskPresent"
+                    )
                 }
-                return isMagiskPresent
+            } catch (e: IOException) {
+                Log.e(TAG, "An IO exception caught.", e)
             }
-    }
-
-    companion object {
-        private val blackListedMountPaths = arrayOf("magisk", "core/mirror", "core/img")
-        private const val TAG = "DetectMagisk-Isolated"
+            return isMagiskPresent
+        }
     }
 }
